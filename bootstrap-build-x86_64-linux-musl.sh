e4d05825aac38abb5d6329e82766ef661a72d32c
Daniel Massey <masseyd1396@gmail.com>
2022-09-01 22:51:36 +0100

    Add convenience script for building

---
+++ bootstrap-build-x86_64-linux-musl.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# Boostraps and builds an x86_64-linux-musl toolchain using config.mak.default.
+
+# Exit on error.
+set -e
+
+# Ensure config.mak.default exists.
+if [ ! -e config.mak.default ]; then
+  echo "Error: config.mak.default does not exist." >&2
+  exit 1
+fi
+
+# Create a temporary directory for storing the bootstrap toolchain.
+tooldir="$(mktemp -d)"
+
+# Download the bootstrap toolchain.
+curl -L https://musl.cc/x86_64-linux-musl-native.tgz -o "${tooldir}"/bstrap.tgz
+
+# Extract the bootstrap toolchain.
+tar -xf "${tooldir}"/bstrap.tgz -C "${tooldir}" --strip-components=1
+
+# Add toolchain bin directory to PATH.
+export PATH="${PATH}:${tooldir}/bin"
+
+# Copy config.mak.default to config.mak.
+cp -f config.mak.default config.mak
+
+# Start the bootstrap build.
+make -j$(nproc)
+
+# Install the bootstrap build.
+make -j1 install
+
+# Overwrite the temporary toolchain with the new build.
+cp -af output/* "${tooldir}"
+rm -rf output
+
+# Clean up the stage 1 build.
+make -j1 distclean
+
+# Start the final build.
+make -j$(nproc)
+
+# Install the final build.
+make -j1 install
+
+# Package the tarball.
+cd output
+tar -cJvf ../x86_64-linux-musl-toolchain.tar.xz *
+cd ..
+
+# Clean up.
+make -j1 distclean
+rm -rf output
+rm -f config.mak
