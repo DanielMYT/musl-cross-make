cb8fc2256639030e27767319028ef9bd2dbf9ec3
Daniel Massey <masseyd1396@gmail.com>
2023-11-19 00:02:49 +0000

    Update Linux to 6.6
    Linux 6.6 is LTS. Also misc fixes.

--- bootstrap-build-x86_64-linux-musl.sh
+++ bootstrap-build-x86_64-linux-musl.sh
@@ -5,6 +5,12 @@
 # Exit on error.
 set -e
 
+# Ensure we are running on an x86_64 host (todo: cross-compilation?).
+if [ "$(uname -m)" != "x86_64" ]; then
+  echo "Error: This script must be run on an x86_64 host system." >&2
+  exit 1
+fi
+
 # Ensure config.mak.default exists.
 if [ ! -e config.mak.default ]; then
   echo "Error: config.mak.default does not exist." >&2
@@ -15,13 +21,17 @@ fi
 tooldir="$(mktemp -d)"
 
 # Download the bootstrap toolchain.
-curl -L https://more.musl.cc/11-20211120/x86_64-linux-musl/x86_64-linux-musl-native.tgz -o "${tooldir}"/bstrap.tgz
+curl -L https://musl.cc/x86_64-linux-musl-native.tgz -o "${tooldir}"/bstrap.tgz
 
 # Extract the bootstrap toolchain.
 tar -xf "${tooldir}"/bstrap.tgz -C "${tooldir}" --strip-components=1
 
 # Add toolchain bin directory to PATH.
 export PATH="${PATH}:${tooldir}/bin"
+
+# Cleanup first.
+make -j1 distclean
+rm -rf x86_64-linux-musl-toolchain
 
 # Copy config.mak.default to config.mak.
 cp -f config.mak.default config.mak
@@ -46,11 +56,13 @@ make -j$(nproc)
 make -j1 install
 
 # Package the tarball.
-cd output
-tar -cJvf ../x86_64-linux-musl-toolchain.tar.xz *
-cd ..
+mv output x86_64-linux-musl-toolchain
+tar -cJvf x86_64-linux-musl-toolchain.tar.xz x86_64-linux-musl-toolchain
 
 # Clean up.
 make -j1 distclean
-rm -rf output
+rm -rf x86_64-linux-musl-toolchain
 rm -f config.mak
+
+# Final message.
+echo -e "\nDone. Output tarball is 'x86_64-linux-musl-toolchain.tar.xz'."
